e24756d633c3290586eb42345db2e1176779afe4
Calin Mihai <32879010+CalinMihai@users.noreply.github.com>
2017-10-25 09:04:54 +0300

    Update I.3.c

--- I.3.c
+++ I.3.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-int sequence[100], Max, Min, i;
+int sequence[100], Max, Min, i, n;
 int main()
 {
+    scanf("%d", &n);
 Max = INT_MIN;
 Min = INT_MAX;
-    for(i = 1; i <= 100; i++){
+    for(i = 1; i <= n; i++){
     scanf("%d", &sequence[i]);
     if(Max < sequence[i])
         Max = sequence[i];
-    if(Min > sequence[i]);
+    if(Min > sequence[i])
         Min = sequence[i];
     }
     printf("The largest element of the sequence is %d\n", Max);
